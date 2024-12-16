276b044886b4cd30493c578d8f458fbca3b14e22
Biriki22 <markel.neila.2004@gmail.com>
2024-12-16 08:25:15 +0100

    commit inicial clase empleado

---
+++ src/paquetegit/Empleado.java
@@ -0,0 +1,10 @@
+package paquetegit;
+
+public class Empleado {
+	private String dni;
+	private String nombre;
+	private String apellido;
+	private int edad;
+	private float sueldo;
+	private boolean hijos; //True si tiene hijos False no tiene hijos
+}
